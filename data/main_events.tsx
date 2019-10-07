2138002d7727d3ffa7b285222b3a71cbb549a35c
VectorWolf <mr.joe63@googlemail.com>
2019-10-07 02:57:37 +0200

    Finish second and third map

--- data/main_events.tsx
+++ data/main_events.tsx
@@ -4,7 +4,7 @@
  <tile id="0" type="GeChangeMap">
   <properties>
    <property name="NAME" value="load_map"/>
-   <property name="PATH" type="file" value="maps/title_screen.tmx"/>
+   <property name="PATH" type="file" value="maps/map3.tmx"/>
   </properties>
  </tile>
 </tileset>
